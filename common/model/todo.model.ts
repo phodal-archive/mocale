d2716b93918a51e63db730df50b87cf2126bbf40
Phodal HUANG <h@phodal.com>
2019-07-19 11:03:27 +0800

    common: update todo model

--- common/model/todo.model.ts
+++ common/model/todo.model.ts
@@ -1,3 +1,14 @@
+export enum REPEAT_TYPE {
+  ONE_TIME,
+  DAILY,
+  MONDAY_TO_FRIDAY,
+  EVERY_WEEK,
+  EVERY_MONTH_WEEKEND,
+  EVERY_MONTH_DAY,
+  EVERY_YEAR,
+  CUSTOM,
+}
+
 export interface TodoModel {
   id?: string;
   title: string;
@@ -5,4 +16,8 @@ export interface TodoModel {
   completed: boolean;
   createdAt?: number;
   modifiedAt?: number;
+  startDate?: string;
+  endDate?: string;
+  repeat?: REPEAT_TYPE;
+  repeatDetail?: string;
 }
