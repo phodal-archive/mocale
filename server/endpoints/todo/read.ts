136c574f6173681fbc0204e361285a230fca69c7
Phodal HUANG <h@phodal.com>
2019-07-19 08:24:41 +0800

    server: add basic get api

--- server/endpoints/todo/read.ts
+++ server/endpoints/todo/read.ts
@@ -1,13 +1,35 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, Callback, Context } from 'aws-lambda';
 import 'source-map-support/register';
 
-export const list: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+const AWS = require('aws-sdk');
+
+AWS.config.setPromisesDependency(Promise);
+
+const tableName = process.env.DYNAMODB_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+export const list: APIGatewayProxyHandler = (_event: APIGatewayProxyEvent, _context: Context, callback: Callback) => {
+  const params = {
+    TableName: tableName
   };
+
+  docClient.scan(params, (error, data) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {'Content-Type': 'text/plain'},
+        body: 'couldn\'t validate the form item.',
+      });
+    }
+
+    callback(null, {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+      },
+      body: JSON.stringify(data),
+    });
+  });
 };
 
